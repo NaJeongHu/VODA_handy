2576cdea0c1357a049a0e94c0165903ead8d2a81
NaJeongHu <65152311+NaJeongHu@users.noreply.github.com>
2021-10-16 00:02:57 +0900

    zzz

--- app/src/main/java/com/example/voda_handy/DetailedMenuActivity.java
+++ app/src/main/java/com/example/voda_handy/DetailedMenuActivity.java
@@ -5,16 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import java.util.ArrayList;
 
 public class DetailedMenuActivity extends AppCompatActivity {
 
     private Menu menu;
+    private FirebaseAuth mFirebaseAuth;     // 파이어베이스 인증
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed_menu);
         init();
+
     }
 
     private void init() {
--- app/src/main/java/com/example/voda_handy/RegisterActivity.java
+++ app/src/main/java/com/example/voda_handy/RegisterActivity.java
@@ -3,26 +3,43 @@ package com.example.voda_handy;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth mFirebaseAuth;     // 파이어베이스 인증
     private DatabaseReference mDatabaseRef; // 실시간 데이터베이스
     private EditText mEtEmail, mEtPassword;
+    private ImageView mIvPicture;
+    private Uri imageUri;
+    private StorageTask uploadTask;
+    private StorageReference storageReference;
+    private static final int IMAGE_REQUEST = 0;
     private Button mBtnRegister;
 
     @Override
@@ -40,6 +57,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         mEtEmail = findViewById(R.id.et_email);
         mEtPassword = findViewById(R.id.et_password);
         mBtnRegister = findViewById(R.id.btn_register);
+        mIvPicture = findViewById(R.id.iv_picture);
 
         mBtnRegister.setOnClickListener(this);
     }
@@ -73,9 +91,81 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                     }
                 });
                 break;
+            case R.id.iv_picture:
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, IMAGE_REQUEST);
+                break;
 
             default:
                 break;
+        }
+    }
+
+    private void uploadImage() {
+
+        if(imageUri != null) { //intent의 결과로 imageUri가 넘어왔다면,
+            final StorageReference fileReference = storageReference.child(System.currentTimeMillis()
+                    +"."+getFileExtension(imageUri));
+
+            uploadTask = fileReference.putFile(imageUri);
+            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if(!task.isSuccessful()) {
+                        throw task.getException();
+                    }
+
+                    return fileReference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if(task.isSuccessful()){
+                        Uri downloadUri = task.getResult();
+                        String mUri = downloadUri.toString();
+
+//                        mDatabaseRef = FirebaseDatabase.getInstance().getReference("Users").child(fuser.getUid());
+                        HashMap<String, Object> map = new HashMap<>();
+                        map.put("imageURL", mUri);
+                        mDatabaseRef.updateChildren(map);
+                    } else {
+                        Toast.makeText(RegisterActivity.this, "Failed!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(RegisterActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            Toast.makeText(RegisterActivity.this, "선택된 이미지가 없습니다", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver contentResolver = RegisterActivity.this.getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == IMAGE_REQUEST){
+            if(resultCode == RESULT_OK && data != null && data.getData() != null) {
+                imageUri = data.getData();
+
+                //set Image to mIvPicture
+                if (imageUri != null) {
+                    mIvPicture.setImageURI(imageUri);
+                }
+            } else if(resultCode == RESULT_CANCELED) {
+//                Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
